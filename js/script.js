5ed659d07ac3ea18960a9a68e99f945f616482e2
Tanvir Ahmed <tanvirahmedonly@gmail.com>
2022-07-04 14:40:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // popup
+    // Magnific - Popup
+    $(' .popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+});
+
+
+
+// mixitup
+var mixer = mixitup('.mymixup');
